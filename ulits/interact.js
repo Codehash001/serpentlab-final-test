e31bf8dc4880cb34c24a417b94f6813e7ba2a9fd
Codehash001 <111737745+Codehash001@users.noreply.github.com>
2022-10-19 22:14:49 +0530

    Update interact.js

--- ulits/interact.js
+++ ulits/interact.js
@@ -10,7 +10,7 @@ import { config } from '../dapp.config'
 
 const web3 = createAlchemyWeb3(process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL)
 
-const contract = require('../artifacts/contracts/Test4.sol/Test4.json')
+const contract = require('../artifacts/contracts/SerpentLabs.sol/SerpentLabs.json')
 const nftContract = new web3.eth.Contract(contract.abi, config.contractAddress)
 
 // Calculate merkle root from the whitelist array
